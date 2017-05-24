5133010b75389859982d4600f77341ed9e22ffe7
Jochem <jochem@degoede.email>
2017-05-24 11:56:44 +0200

    Updated make_dataset.py to fix Pylint issues

--- {{ cookiecutter.repo_name }}/src/data/make_dataset.py
+++ {{ cookiecutter.repo_name }}/src/data/make_dataset.py
@@ -1,7 +1,9 @@
+# pylint: disable=unexpected-keyword-arg, no-value-for-parameter
 # -*- coding: utf-8 -*-
 import os
-import click
 import logging
+
+import click
 from dotenv import find_dotenv, load_dotenv
 
 
@@ -17,11 +19,11 @@ def main(input_filepath, output_filepath):
 
 
 if __name__ == '__main__':
-    log_fmt = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
-    logging.basicConfig(level=logging.INFO, format=log_fmt)
+    LOG_FMT = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+    logging.basicConfig(level=logging.INFO, format=LOG_FMT)
 
     # not used in this stub but often useful for finding various files
-    project_dir = os.path.join(os.path.dirname(__file__), os.pardir, os.pardir)
+    PROJECT_DIR = os.path.join(os.path.dirname(__file__), os.pardir, os.pardir)
 
     # find .env automagically by walking up directories until it's found, then
     # load up the .env entries as environment variables
